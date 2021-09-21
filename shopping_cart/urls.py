45b8dc6cd9a84ae3a963622c410dffda37e09337
Serhii Sliadniev <s.sliadniev@quantumobile.com>
2021-09-21 12:02:26 +0300

    16586-feature/UserApp: add Swagger

--- shopping_cart/urls.py
+++ shopping_cart/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
 
+schema_view = get_swagger_view(title='Shopping Cart_Swagger')
 
 main_urls = [
     path('admin/', admin.site.urls),
@@ -9,5 +11,6 @@ main_urls = [
 
 urlpatterns = [
     path('auth/', include('rest_auth.urls')),
-    path('auth/registration/', include('rest_auth.registration.urls'))
+    path('auth/registration/', include('rest_auth.registration.urls')),
+    path('', schema_view)
 ]
