99892f11f07b95ceb94c44c83b24957f2d7c77c8
Serhii Sliadniev <s.sliadniev@quantumobile.com>
2021-09-21 10:29:42 +0300

    16485-feature/UserApp: add field for photo in User model

--- user/models.py
+++ user/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     phone = models.CharField(max_length=20)
     address = models.CharField(max_length=70)
-    # add photow
+    photo = models.ImageField(upload_to='images')
 
     def __str__(self):
         return f"{self.first_name}, {self.last_name}"
