b18480e9946946fd994daedaddcadaa79e09d3e5
Serhii Sliadniev <s.sliadniev@quantumobile.com>
2021-09-27 11:18:53 +0300

    feature/UserApp: fix views/tasks/tests

--- tasks.py
+++ tasks.py
@@ -3,9 +3,8 @@ from invoke import task
 
 @task
 def run_local(ctx):
-    # collect static
+    ctx.run('python3 manage.py collectstatic')
     # download dump DB
-
     ctx.run("python3 ./manage.py makemigrations")
     ctx.run("python3 ./manage.py migrate")
     ctx.run("python3 ./manage.py runserver 0.0.0.0:8000")
--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user
 from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 
@@ -6,10 +7,10 @@ from .serializers import UserSerializer
 
 
 class CurrentUserRetrieveUpdateAPIView(RetrieveUpdateAPIView):
-    queryset = User.objects.all()
+   # queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticated,)
 
     def get_object(self):
-        return self.request.user
+        return get_user(self.request)
 
