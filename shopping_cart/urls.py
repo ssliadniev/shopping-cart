d3d87a00dc6f11f602d0833e6fc2194e32a7a6be
Serhii Sliadniev <88987046+ssliadniev@users.noreply.github.com>
2021-09-27 13:02:12 +0300

    Revert "Feature/user app: fix in progress"

--- shopping_cart/urls.py
+++ shopping_cart/urls.py
@@ -1,17 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework_swagger.views import get_swagger_view
+from django.urls import path
 
-schema_view = get_swagger_view(title='Shopping Cart_Swagger')
-
-main_urls = [
-    path('user/', include('user.urls'))
-]
+from .views import CurrentUser
 
 urlpatterns = [
-    path('auth/', include('rest_auth.urls')),
-    path('auth/registration/', include('rest_auth.registration.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include(main_urls)),
-    path('swagger/', schema_view)
+    path('/user/', CurrentUser.as_view()),
 ]
