712fa602a1efcbd1c083a928644764cb620a9136
Serhii Sliadniev <s.sliadniev@quantumobile.com>
2021-09-30 18:01:44 +0300

    feature/UserApp: fix product app

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import Category, Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Information about product',
+         {'fields': ['title', 'description', 'in_stock', 'slug', 'price']})
+    ]
+    list_display = ('title', 'description', 'in_stock', 'slug', 'price')
+    list_filter = ['in_stock']
+    search_fields = ['title', 'slug']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Information about category',
+         {'fields': ['title', 'description']})
+    ]
+    list_display = ('title', 'description')
+    list_filter = ['title']
+    search_fields = ['title']
+    actions = ['delete_selected']
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def delete_selected(self, request, queryset):
+        queryset.update(is_abort=True)
+
+    delete_selected.short_description = "Delete selected item"
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -5,10 +5,16 @@ class Product(models):
     title = models.CharField(max_length=55)
     description = models.TextField()
     in_stock = models.BooleanField(blank=False)
-    slug = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=255, unique=True, allow_unicode=True)
     price = models.FloatField()
+
+    def __str__(self):
+        return self.title
 
 
 class Category(models):
     title = models.CharField(max_length=40)
     description = models.TextField()
+
+    def __str__(self):
+        return self.title
