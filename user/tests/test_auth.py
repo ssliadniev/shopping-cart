fcafb61400728cc96a77dc31ade36a2ad9fffdb8
Serhii Sliadniev <s.sliadniev@quantumobile.com>
2021-10-03 14:59:30 +0300

    feature/UserApp: fix tests for user app

--- user/tests/test_auth.py
+++ user/tests/test_auth.py
@@ -6,6 +6,23 @@ from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED,  HTTP_201_CREAT
 class AuthTest(TransactionTestCase):
 
     def test_register_successful(self):
+        data = {
+            ""
+        }
         register_url = reverse("register")
-        response = self.client.get(register_url)
+        response = self.client.post(register_url, data)
         self.assertEqual(response.status_code, HTTP_200_OK)
+
+    def test_register_failed(self):
+        data = {
+            ""
+        }
+        register_url = reverse("register")
+        response = self.client.post(register_url, data)
+        self.assertEqual(response.status_code, HTTP_200_OK)
+
+    def test_login_successful(self):
+        pass
+
+    def test_login_failed(self):
+        pass
--- user/tests/test_current_user.py
+++ user/tests/test_current_user.py
@@ -1,16 +1,23 @@
 from django.contrib.auth.models import User
 from django.test import TransactionTestCase
 from django.urls import reverse
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_401_UNAUTHORIZED
 
 
 class CurrentUserTestcase(TransactionTestCase):
     fixtures = ["user.json"]
 
-    def test_get_current_user_success(self):
+    def test_get_current_user_failed(self):
         url = reverse("user")
         response = self.client.get(url)
-        self.assertEqual(response.status_code, HTTP_200_OK)
+        self.assertEqual(response.status_code, HTTP_401_UNAUTHORIZED)
 
-    def test_update_current_user_success(self):
+    def test_get_current_user_success(self):
+        expected_data = {}
+        # force_login
+        # check status.code and check data
+        # response.data
+        pass
+
+    def test_update_current_user_failed(self):
         pass
