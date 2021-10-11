935fa9971fe65b6ad1d5460fab6ce011388b4081
Serhii Sliadniev <s.sliadniev@quantumobile.com>
2021-10-11 11:10:24 +0300

    feature/ProductsApp: add products app

---
+++ products/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(max_length=150, unique=True, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name', )
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, db_index=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    in_stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.title
