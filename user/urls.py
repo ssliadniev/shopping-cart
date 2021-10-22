813a8b23ecf13c39c043f6a763400dc6ea63acc0
Serhii Sliadniev <s.sliadniev@quantumobile.com>
2021-10-22 10:18:33 +0300

    ShoppingCart: add app name for user urls

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from .views import CurrentUserRetrieveUpdateAPIView
 
+app_name = "user"
+
 urlpatterns = [
     path('', CurrentUserRetrieveUpdateAPIView.as_view(), name="user"),
 ]
