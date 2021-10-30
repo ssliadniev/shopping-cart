4882292aee5e2a6ccf508eb2ec2ce7a8c1a63e94
Serhii Sliadniev <s.sliadniev@quantumobile.com>
2021-10-30 23:14:39 +0300

    ProductsApp: fix product serializer

--- products/serializers.py
+++ products/serializers.py
@@ -5,10 +5,18 @@ from rest_framework import serializers
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ('title', 'slug', 'created_at', 'update_at')
+        fields = ('pk', 'title', 'slug')
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('category', 'title', 'slug', 'description', 'price', 'available')
+        fields = ('pk', 'category', 'title', 'slug', 'description', 'price', 'available')
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret['category'] = {
+            "pk": instance.category.pk,
+            "title": instance.category.title
+        }
+        return ret
