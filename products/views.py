e469117017e71f368774f0b0588c9775395fdb24
Serhii Sliadniev <s.sliadniev@quantumobile.com>
2021-10-30 23:15:22 +0300

    ProductsApp: fix views for product app

--- products/views.py
+++ products/views.py
@@ -10,13 +10,26 @@ class CategoryListCreateAPIView(ListCreateAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
 
+class CategoryRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
 class ProductListCreateAPIView(ListCreateAPIView):
-    queryset = Product.objects.filter(available=True)
+    queryset = Product.objects.all(available=True)
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        pass
 
 
 class ProductRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        pass
+
