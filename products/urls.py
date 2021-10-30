75d80d3577c48fbc6671ccc74000addd5009c87a
Serhii Sliadniev <s.sliadniev@quantumobile.com>
2021-10-30 23:21:03 +0300

    ProductsApp: fix urls for products app

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
+from .views import CategoryListCreateAPIView, ProductListCreateAPIView,  ProductRetrieveUpdateDestroyAPIView
+
+
 urlpatterns = [
-    path(''),
-    path('<id>/'),
-    path('categories/'),
-    path('categories/<id>/')
+    path('', ProductListCreateAPIView.as_view(), name="list-create"),
+    path('<pk:int>/', ProductRetrieveUpdateDestroyAPIView.as_view(), name="ret...."),  # pk or int first?
+    path('categories/', CategoryListCreateAPIView.as_view(), name="categories-list..."),
+    path('categories/<id>/',)
 ]
