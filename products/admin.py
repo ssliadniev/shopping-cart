12e0059642959ed95b966fffc7f7af7f889d9e7a
Serhii Sliadniev <s.sliadniev@quantumobile.com>
2021-10-31 22:42:24 +0200

    ProductsApp: add product/category admin classes

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import Product, Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = ("title", "category", "available", "in_stock")
+    list_filter = ["category"]
+    search_fields = (
+        "title",
+        "category",
+        "in_stock"
+    )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ("title", "created_at", "updated_at")
+    list_filter = ["title"]
+    search_fields = (
+        "title",
+    )
