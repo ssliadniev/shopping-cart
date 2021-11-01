c5ceba3288bd352bfe622084c973bee069d0d4ee
Serhii Sliadniev <s.sliadniev@quantumobile.com>
2021-11-01 10:27:25 +0200

    ProductsApp: add ProductRetrieveUpdateDestroyAPIView

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,25 @@
 from django.urls import path
 
-from .views import CategoryListCreateAPIView, ProductListCreateAPIView,  ProductRetrieveUpdateDestroyAPIView
-
+from .views import (CategoryListCreateAPIView,
+                    CategoryRetrieveUpdateDestroyAPIView,
+                    ProductListCreateAPIView,
+                    ProductRetrieveUpdateDestroyAPIView)
 
 urlpatterns = [
-    path('', ProductListCreateAPIView.as_view(), name="list-create"),
-    path('<int:pk>/', ProductRetrieveUpdateDestroyAPIView.as_view(), name="retrieve-update-destroy"),
-    path('categories/', CategoryListCreateAPIView.as_view(), name="categories-list-create"),
-    path('categories/<int:pk>/', ProductRetrieveUpdateDestroyAPIView.as_view(), name="retrieve-update-destroy")
+    path("", ProductListCreateAPIView.as_view(), name="list-create"),
+    path(
+        "<int:pk>/",
+        ProductRetrieveUpdateDestroyAPIView.as_view(),
+        name="product-retrieve-update-destroy",
+    ),
+    path(
+        "categories/",
+        CategoryListCreateAPIView.as_view(),
+        name="category-list-create",
+    ),
+    path(
+        "categories/<int:pk>/",
+        CategoryRetrieveUpdateDestroyAPIView.as_view(),
+        name="category-retrieve-update-destroy",
+    ),
 ]
