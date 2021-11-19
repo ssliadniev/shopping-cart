4a603315abca2792cf1127c9dec48bb3910c2dde
Serhii Sliadniev <s.sliadniev@quantumobile.com>
2021-11-19 14:46:21 +0200

    feature/ProductsApp: fix views

--- products/views.py
+++ products/views.py
@@ -1,35 +1,42 @@
 from .models import Category, Product
+from rest_framework import filters
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from serializers import CategorySerializer, ProductSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+from products.serializers import CategorySerializer, ProductSerializer
+from products.permissions import IsAdminUserOrReadOnly
+from products.filters import ProductFilters
 
 
 class CategoryListCreateAPIView(ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminUserOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["title"]
 
 
 class CategoryRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminUserOrReadOnly]
+
+
+class AdminProductListCreateAPIView(ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAdminUser]
 
 
 class ProductListCreateAPIView(ListCreateAPIView):
-    queryset = Product.objects.all(available=True)
+    queryset = Product.objects.filter(available=True)
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-
-    def get_queryset(self):
-        pass
+    permission_classes = [IsAdminUserOrReadOnly]
+    filter_backends = [filters.SearchFilter, filters.BaseFilterBackend]
+    filter_class = ProductFilters
 
 
 class ProductRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-
-    def get_queryset(self):
-        pass
+    permission_classes = [IsAdminUserOrReadOnly]
 
