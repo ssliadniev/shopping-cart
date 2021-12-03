5842da54e05565ab1ef8adeacfd770d4933130f6
Serhii Sliadniev <s.sliadniev@quantumobile.com>
2021-12-04 00:25:06 +0200

    feature/ProductsApp: add filter_in_stock

--- products/filters.py
+++ products/filters.py
@@ -7,17 +7,24 @@ class ProductFilters(django_filters.FilterSet):
     price_from = django_filters.NumberFilter(field_name="price", lookup_expr="gte")
     price_to = django_filters.NumberFilter(field_name="price", lookup_expr="lte")
     search = django_filters.CharFilter(method="filter_search")
+    is_in_stock = django_filters.BooleanFilter(method="filter_in_stock")
 
     class Meta:
         model = Product
-        fields = ["in_stock", "price_from", "price_to", "search"]
+        fields = ["price_from", "price_to", "search", "is_in_stock"]
 
     @staticmethod
     def filter_search(queryset, values, *args, **kwargs):
         values = values.split(" ") if values else []
         for value in values:
             queryset = queryset.filter(
-                Q(title__incontains=value) |
-                Q(category__title_incontains=value)
+                Q(title__icontains=value) |
+                Q(category__title__icontains=value)
             )
         return queryset
+
+    @staticmethod
+    def filter_in_stock(queryset, value, *args, **kwargs):
+        if value:
+            return queryset.filter(in_stock__gt=0)
+        return queryset.filter(in_stock=0)
