76217bcfb3bd46809495549646f697ed91236c35
Serhii Sliadniev <s.sliadniev@quantumobile.com>
2021-12-16 10:19:53 +0200

    feature/Cart: add Cart/CartItem  models

--- cart/models.py
+++ cart/models.py
@@ -1,25 +1,35 @@
 from django.db import models
+
+from products.models import Product
 from user.models import User
-from user.models import Product
 
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
-        ordering = ('user',)
+        ordering = ("user",)
+
+    def total(self):
+        total_prod = CartItem.objects.filter(cart=self)
+        return sum([i.product.price * i.quantity for i in total_prod])
 
     def __str__(self):
         return self.user
 
 
 class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name="items", on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
-    price = models.FloatField(blank=True)
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
 
     class Meta:
-        verbose_name = 'item'
-        verbose_name_plural = 'items'
-        ordering = ('cart',)
+        verbose_name = "item"
+        verbose_name_plural = "items"
+        ordering = ("cart",)
+
+    def get_sub_total(self):
+        return self.product.price * self.quantity
+
+    def __str__(self):
+        return "{}: {}".format(self.cart, self.product)
