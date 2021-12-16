9f21daaebcc2c1030da74d3c2b84be9856e871ce
Serhii Sliadniev <s.sliadniev@quantumobile.com>
2021-12-16 10:13:47 +0200

    feature/Cart: add urls for cart

--- cart/urls.py
+++ cart/urls.py
@@ -1,26 +1,14 @@
 from django.urls import path
 
-from cart.views import (CartItemListCreateAPIView,
-                        CartItemRetrieveUpdateDestroyAPIView,
-                        CartListCreateAPIView,
-                        CartRetrieveUpdateDestroyAPIView)
+from cart.views import CartItemListCreateAPIView, CartItemUpdateDestroyAPIView
+
 
 app_name = "cart"
 
 
 urlpatterns = [
-    path("cart/", CartListCreateAPIView.as_view(), name="cart-list-create"),
+    path("", CartItemListCreateAPIView.as_view(), name="cart_item-list-create"),
     path(
-        "<int:pk>/",
-        CartRetrieveUpdateDestroyAPIView.as_view(),
-        name="cart-retrieve-update-destroy",
-    ),
-    path(
-        "cart_item/", CartItemListCreateAPIView.as_view(), name="cart_item-list-create"
-    ),
-    path(
-        "cart_item/<int:pk>/",
-        CartItemRetrieveUpdateDestroyAPIView.as_view(),
-        name="cart_item-retrieve-update-destroy",
+        "<int:pk>/", CartItemUpdateDestroyAPIView.as_view(), name="cart_item-update-destroy"
     ),
 ]
