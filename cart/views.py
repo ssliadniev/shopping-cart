81df6bf4d2bcd4db09ef51ef8378f7b709a69308
Serhii Sliadniev <s.sliadniev@quantumobile.com>
2021-12-16 10:36:48 +0200

    feature/Cart: add views for cart app

--- cart/views.py
+++ cart/views.py
@@ -1,11 +1,23 @@
 from rest_framework.generics import ListCreateAPIView, UpdateAPIView, DestroyAPIView
 from cart.models import Cart, CartItem
+from cart.serializers import CartItemSerializer
 
 
 class CartItemListCreateAPIView(ListCreateAPIView):
+    serializer_class = CartItemSerializer
     queryset = CartItem.objects.all()
 
-    #total
+    def get_queryset(self):
+        return self.queryset.filter(cart__user=self.request.user)
+
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        #return Response({"result": serializer.data, "total_amount": queryset.user})
+
 
 class CartItemUpdateDestroyAPIView(UpdateAPIView, DestroyAPIView):
+    serializer_class = CartItemSerializer
     queryset = CartItem.objects.all()
+
+    def get_queryset(self):
+        return self.queryset.filter(cart__user=self.request.user)
