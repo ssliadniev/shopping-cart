f90e910db303b27cdbd52307613ca1166916f7fb
Serhii Sliadniev <s.sliadniev@quantumobile.com>
2022-01-29 19:43:52 +0200

    feature/Cart: fix total() method in Cart model

--- cart/models.py
+++ cart/models.py
@@ -5,22 +5,22 @@ from user.models import User
 
 
 class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="cart")
 
     class Meta:
         ordering = ("user",)
 
     def total(self):
         total_prod = CartItem.objects.filter(cart=self)
-        return sum([i.product.price * i.quantity for i in total_prod])
+        return sum([cart_item.get_sub_total() for cart_item in total_prod])
 
     def __str__(self):
         return self.user
 
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, related_name="items", on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="cart_items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="cart_items")
     quantity = models.PositiveIntegerField(default=1)
 
     class Meta:
