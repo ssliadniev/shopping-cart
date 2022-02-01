de89396666cf562adcb83b0c5290ac1a5cafc09b
Serhii Sliadniev <s.sliadniev@quantumobile.com>
2022-02-01 23:10:07 +0200

    ShoppingCart: make migration

---
+++ cart/migrations/0001_initial.py
@@ -0,0 +1,85 @@
+import django.db.models.deletion
+from django.conf import settings
+from django.db import migrations, models
+
+
+def create_cart_for_user(apps, schema_editor):
+    users = apps.get_model("user", "User")
+    cart = apps.get_model("cart", "Cart")
+    for user in users.objects.all():
+        cart.objects.create(user__id=user.id)
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        ("products", "0001_initial"),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name="Cart",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "user",
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="cart",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
+            ],
+            options={
+                "ordering": ("user",),
+            },
+        ),
+        migrations.CreateModel(
+            name="CartItem",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("quantity", models.PositiveIntegerField(default=1)),
+                (
+                    "cart",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="cart_items",
+                        to="cart.cart",
+                    ),
+                ),
+                (
+                    "product",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="cart_items",
+                        to="products.product",
+                    ),
+                ),
+            ],
+            options={
+                "verbose_name": "item",
+                "verbose_name_plural": "items",
+                "ordering": ("cart",),
+            },
+        ),
+        migrations.RunPython(create_cart_for_user),
+    ]
